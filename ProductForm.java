b401312d9d984619a0a01f8d87dbe836aa532ff7
Polobear9 <lieams1996@gmail.com>
2022-11-10 14:08:06 +0900

    Added study java , servlet file.

---
+++ ProductForm.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+
+public class ProductForm{
+        private String productId;
+        private String productName;
+        private String productIntroduction;
+        private String productClasses;
+        private String productSellPrice;
+        private String productOrderPrice;
+        private String[] sellersShopName;
+        private String addedYear;
+        private String addedMonth;
+        private String addedDay;
+
+        public String getProductId() {
+            return productId;
+        }
+        public void setProductId(String productId) {
+            this.productId = productId;
+        }
+        public String getProductName() {
+            return productName;
+        }
+        public void setProductName(String productName) {
+            this.productName = productName;
+        }
+        public String getProductIntroduction() {
+            return productIntroduction;
+        }
+        public void setProductIntroduction(String productIntroduction) {
+            this.productIntroduction = productIntroduction;
+        }
+        public String getProductClasses() {
+            return productClasses;
+        }
+        public void setProductClasses(String productClasses) {
+            this.productClasses = productClasses;
+        }
+        public String getProductSellPrice() {
+            return productSellPrice;
+        }
+        public void setProductSellPrice(String productSellPrice) {
+            this.productSellPrice = productSellPrice;
+        }
+        public String getProductOrderPrice() {
+            return productOrderPrice;
+        }
+        public void setProductOrderPrice(String productOrderPrice) {
+            this.productOrderPrice = productOrderPrice;
+        }
+        public String getSellersShopName() {
+            return Arrays.toString(sellersShopName);
+        }
+        public void setSellersShopName(String[] sellersShopName) {
+            this.sellersShopName = sellersShopName;
+        }
+        public String getAddedYear() {
+            return addedYear;
+        }
+        public void setAddedYear(String addedYear) {
+            this.addedYear = addedYear;
+        }
+        public String getAddedMonth() {
+            return addedMonth;
+        }
+        public void setAddedMonth(String addedMonth) {
+            this.addedMonth = addedMonth;
+        }
+        public String getAddedDay() {
+            return addedDay;
+        }
+        public void setAddedDay(String addedDay) {
+            this.addedDay = addedDay;
+        }
+        public String getAddedDate() {
+            return getAddedYear() + "年"
+                    + getAddedMonth() + "月"
+                    + getAddedDay() + "日";
+        }
+    }
