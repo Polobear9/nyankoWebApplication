b66e8bf7487e96ff735417c7aab60a05bb17456d
Polobear9 <lieams1996@gmail.com>
2022-11-11 16:19:17 +0900

    Added Files. the java file is base on JSP and JavaServlet not a spring.

---
+++ ProductCompleteServlet.java
@@ -0,0 +1,78 @@
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.asaka.form.ProductForm;
+
+/**
+ * Servlet implementation class ProductConfirmationServlet
+ */
+@WebServlet("/ProductCompleteServlet")
+public class ProductCompleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductCompleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProductForm form = getForm(request);
+		request.setAttribute("productForm", form);
+		request.setCharacterEncoding("UTF-8");
+		if(request.getParameter("action").equals("register")) {
+			RequestDispatcher result = request.getRequestDispatcher("/productComplete.jsp");
+			result.forward(request, response);
+		}else if(request.getParameter("action").equals("back")) {
+			RequestDispatcher result = request.getRequestDispatcher("/productinput.jsp");
+			result.forward(request, response);	
+		}
+	}
+	
+	private ProductForm getForm(HttpServletRequest request) {
+		//リクエストパラメータをフォームに詰め替える
+		String id = request.getParameter("productId");
+		String name = request.getParameter("productName");
+		String detail = request.getParameter("productIntroduction");
+		String category = request.getParameter("productClasses");
+		String salesPrice = request.getParameter("productSellPrice");
+		String purchesePrice = request.getParameter("productOrderPrice");
+		String[] salesStoreIds = request.getParameterValues("sellersShopName");
+		String registrationYear = request.getParameter("addedYear");
+		String registrationMonth = request.getParameter("addedMonth");
+		String registrationDay = request.getParameter("addedDay");
+		
+		ProductForm form = new ProductForm();
+		form.setProductId(id);
+		form.setProductName(name);
+		form.setProductIntroduction(detail);
+		form.setProductClasses(category);
+		form.setProductSellPrice(salesPrice);
+		form.setProductOrderPrice(purchesePrice);
+		form.setSellersShopName(salesStoreIds);
+		form.setAddedYear(registrationYear);
+		form.setAddedMonth(registrationMonth);
+		form.setAddedDay(registrationDay);
+		return form;	
+	}
+
+}
--- ProductForm.java
+++ ProductForm.java
@@ -1,80 +1,100 @@
 import java.util.Arrays;
 
-public class ProductForm{
-        private String productId;
-        private String productName;
-        private String productIntroduction;
-        private String productClasses;
-        private String productSellPrice;
-        private String productOrderPrice;
-        private String[] sellersShopName;
-        private String addedYear;
-        private String addedMonth;
-        private String addedDay;
-
-        public String getProductId() {
-            return productId;
-        }
-        public void setProductId(String productId) {
-            this.productId = productId;
-        }
-        public String getProductName() {
-            return productName;
-        }
-        public void setProductName(String productName) {
-            this.productName = productName;
-        }
-        public String getProductIntroduction() {
-            return productIntroduction;
-        }
-        public void setProductIntroduction(String productIntroduction) {
-            this.productIntroduction = productIntroduction;
-        }
-        public String getProductClasses() {
-            return productClasses;
-        }
-        public void setProductClasses(String productClasses) {
-            this.productClasses = productClasses;
-        }
-        public String getProductSellPrice() {
-            return productSellPrice;
-        }
-        public void setProductSellPrice(String productSellPrice) {
-            this.productSellPrice = productSellPrice;
-        }
-        public String getProductOrderPrice() {
-            return productOrderPrice;
-        }
-        public void setProductOrderPrice(String productOrderPrice) {
-            this.productOrderPrice = productOrderPrice;
-        }
-        public String getSellersShopName() {
-            return Arrays.toString(sellersShopName);
-        }
-        public void setSellersShopName(String[] sellersShopName) {
-            this.sellersShopName = sellersShopName;
-        }
-        public String getAddedYear() {
-            return addedYear;
-        }
-        public void setAddedYear(String addedYear) {
-            this.addedYear = addedYear;
-        }
-        public String getAddedMonth() {
-            return addedMonth;
-        }
-        public void setAddedMonth(String addedMonth) {
-            this.addedMonth = addedMonth;
-        }
-        public String getAddedDay() {
-            return addedDay;
-        }
-        public void setAddedDay(String addedDay) {
-            this.addedDay = addedDay;
-        }
-        public String getAddedDate() {
-            return getAddedYear() + "年"
-                    + getAddedMonth() + "月"
-                    + getAddedDay() + "日";
-        }
-    }
+@SuppressWarnings("unused")
+public class ProductForm {
+	private String productId;
+	private String productName;
+	private String productIntroduction;
+	private String productClasses;
+	private String productSellPrice;
+	private String productOrderPrice;
+	private String[] sellersShopName;
+	private String addedYear;
+	private String addedMonth;
+	private String addedDay;
+	
+	
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductIntroduction() {
+		return productIntroduction;
+	}
+	public void setProductIntroduction(String productIntroduction) {
+		this.productIntroduction = productIntroduction;
+	}
+	public String getProductClasses() {
+		return productClasses;
+	}
+	public void setProductClasses(String productClasses) {
+		this.productClasses = productClasses;
+	}
+	public String getProductSellPrice() {
+		return productSellPrice;
+	}
+	public void setProductSellPrice(String productSellPrice) {
+		this.productSellPrice = productSellPrice;
+	}
+	public String getProductOrderPrice() {
+		return productOrderPrice;
+	}
+	public void setProductOrderPrice(String productOrderPrice) {
+		this.productOrderPrice = productOrderPrice;
+	}
+	public String[] getSellersShopName() {
+		return sellersShopName;
+	}
+	public void setSellersShopName(String[] sellersShopName) {
+		this.sellersShopName = sellersShopName;
+	}
+	public String getAddedYear() {
+		return addedYear;
+	}
+	public void setAddedYear(String addedYear) {
+		this.addedYear = addedYear;
+	}
+	public String getAddedMonth() {
+		return addedMonth;
+	}
+	public void setAddedMonth(String addedMonth) {
+		this.addedMonth = addedMonth;
+	}
+	public String getAddedDay() {
+		return addedDay;
+	}
+	public void setAddedDay(String addedDay) {
+		this.addedDay = addedDay;
+	}
+	public String getAddedDate() {
+		return getAddedYear() + "年"
+				+ getAddedMonth() + "月"
+				+ getAddedDay() + "日";
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ProductForm [productId=").append(productId)
+		.append(", productName=").append(productName)
+		.append(", productIntroduction=").append(productIntroduction)
+		.append(", productClasses=").append(productClasses)
+		.append(", productSellPrice=").append(productSellPrice)
+		.append(", productOrderPrice=").append(productOrderPrice)
+		.append(", sellersShopName=").append(Arrays.toString(sellersShopName))
+		.append(", addedYear=").append(addedYear)
+		.append(", addedMonth=").append(addedMonth)
+		.append(", addedDay=").append(addedDay).append("]");
+		return builder.toString();
+	}
+	
+	
+	
+}
