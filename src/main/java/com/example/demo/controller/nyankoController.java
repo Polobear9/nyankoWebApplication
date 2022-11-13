9701cf4cf72211f3f12a414e9c6cb0bf52bd8683
Polobear9 <lieams1996@gmail.com>
2022-11-13 11:19:48 +0900

    Added and Change the code for spring.

---
+++ src/main/java/com/example/demo/controller/nyankoController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class nyankoController {
+    @PostMapping("/nyankoController")
+    public String nyankoCheck(){
+        System.out.println(input_ShopName);
+        return "";
+    }
+}
