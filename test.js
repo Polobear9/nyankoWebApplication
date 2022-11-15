d3d13abfde98a4026ff8ca78ccaee700b7a99fbb
Polobear9 <lieams1996@gmail.com>
2022-11-16 01:35:47 +0900

    Added REST API sample code.

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
-testMovies.co/api/movies // call the movies. it is be REST API
-testMovies.co/api/movies/1
-testMovies.co/api/search?rating=9 //it is REST API
+GET testMovies.co/api/movies // call the movies. it is be REST API
+POST testMovies.co/api/movies
+
+GET testMovies.co/api/movies/1 //you can get the info about the movies
+PUT testMovies.co/api/movies/1 //you can update the movies about 1.
+
+GET testMovies.co/api/search?rating=9 //it is REST API
+ POST testMovies.co/api/search?rating=9 //you can create a info about the search results 9.
 //REST API are URL so it is so easy to understand.
+//get --> give , post --> update..upload..create..also
+
+GET testapi/user/bookmarks //-- > get info about bookmarks
+POST testapi/user/bookmarks //--> user create the bookmarks
