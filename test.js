b17a3e1ee5c44e9496730979cef9db4f06db42e1
Polobear9 <lieams1996@gmail.com>
2022-11-15 19:43:05 +0900

    Added REST API sample code.

---
+++ test.js
@@ -0,0 +1,4 @@
+testMovies.co/api/movies // call the movies. it is be REST API
+testMovies.co/api/movies/1
+testMovies.co/api/search?rating=9 //it is REST API
+//REST API are URL so it is so easy to understand.
